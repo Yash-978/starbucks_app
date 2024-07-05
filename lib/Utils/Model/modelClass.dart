610560d36c24f778b36bbc21ca86a10ab542ff97
ASUS <5646krupaparmar@gmail.com>
2024-07-05 16:03:05 +0530

    krupa krupa1 123

--- lib/Utils/Model/modelClass.dart
+++ lib/Utils/Model/modelClass.dart
@@ -1,4 +1,12 @@
 class ProductDetailModel
 {
+  String? productName,category,size,celsius,description,ingredients,veg_nonveg;
+  int? price,productNo;
 
+  ProductDetailModel({this.productName,this.category,this.size,this.celsius,this.description,this.ingredients,this.veg_nonveg,this.price,this.productNo});
+
+factory ProductDetailModel.fromProduct(Map m1)
+{
+  return ProductDetailModel(productName: m1['productName'],category: m1['category'],size: m1['size'],celsius: m1['celsius'],description: m1['description'],ingredients: m1['ingredients'],veg_nonveg: m1['veg_nonveg']);
+}
 }
