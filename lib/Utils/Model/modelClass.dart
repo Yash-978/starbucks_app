368702dab8c17278c10a2e63e9bfddab8e89eb81
ASUS <5646krupaparmar@gmail.com>
2024-07-05 19:46:42 +0530

    krupa krupa1 123 55

--- lib/Utils/Model/modelClass.dart
+++ lib/Utils/Model/modelClass.dart
@@ -7,6 +7,15 @@ class ProductDetailModel
 
 factory ProductDetailModel.fromProduct(Map m1)
 {
-  return ProductDetailModel(productName: m1['productName'],category: m1['category'],size: m1['size'],celsius: m1['celsius'],description: m1['description'],ingredients: m1['ingredients'],veg_nonveg: m1['veg_nonveg']);
+  return ProductDetailModel(productName: m1['productName'],category: m1['category'],size: m1['size'],celsius: m1['celsius'],description: m1['description'],ingredients: m1['ingredients'],veg_nonveg: m1['veg_nonveg'],price: m1['price'],productNo: m1['productNo']);
 }
+ List<ProductDetailModel> ProductDetail=[];
+  ProductDetailModel.toList(List l1)
+  {
+    for(int i=0;i<l1.length;i++)
+      {
+        ProductDetail.add(ProductDetailModel.fromProduct(l1[i]));
+      }
+  }
+
 }
